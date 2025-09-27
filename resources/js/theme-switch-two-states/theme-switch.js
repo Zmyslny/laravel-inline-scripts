742486d2239813237deea64f0e1fe9ad0822acec
Ostrowski Maciej <zmyslny@icloud.com>
2025-09-27 19:25:30 +0200

    chore: formating js

--- resources/js/theme-switch-two-states/theme-switch.js
+++ resources/js/theme-switch-two-states/theme-switch.js
@@ -1,17 +1,16 @@
 (function __FUNCTION_NAME__() {
-    document.addEventListener("keydown", (event) => {
-        if (
-            event.key === "__TOGGLE_KEY__" &&
-            !event.ctrlKey &&
-            !event.altKey &&
-            !event.metaKey
-        ) {
-            event.preventDefault();
+  document.addEventListener("keydown", (event) => {
+    if (
+      event.key === "__TOGGLE_KEY__" &&
+      !event.ctrlKey &&
+      !event.altKey &&
+      !event.metaKey
+    ) {
+      event.preventDefault();
 
-            const isDark =
-                document.documentElement.classList.toggle("__DARK__");
+      const isDark = document.documentElement.classList.toggle("__DARK__");
 
-            localStorage.theme = isDark ? "__DARK__" : "__LIGHT__";
-        }
-    });
+      localStorage.theme = isDark ? "__DARK__" : "__LIGHT__";
+    }
+  });
 })();
