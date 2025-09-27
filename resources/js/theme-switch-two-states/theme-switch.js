17343c291e3a278beb1f362a6d2dc25b292495ff
Ostrowski Maciej <zmyslny@icloud.com>
2025-09-27 21:39:07 +0200

    chore: todo

--- resources/js/theme-switch-two-states/theme-switch.js
+++ resources/js/theme-switch-two-states/theme-switch.js
@@ -1,6 +1,7 @@
 (function __FUNCTION_NAME__() {
   document.addEventListener("keydown", (event) => {
     if (
+      // @todo Check if focus is on an input, textarea, or select element to avoid conflicts with typing
       event.key === "__TOGGLE_KEY__" &&
       !event.ctrlKey &&
       !event.altKey &&
