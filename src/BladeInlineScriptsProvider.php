0a623a84042b107dc149158688a855f016ba5101
Ostrowski Maciej <zmyslny@icloud.com>
2025-10-08 23:04:01 +0200

    refactor: names

--- src/BladeInlineScriptsProvider.php
+++ src/BladeInlineScriptsProvider.php
@@ -28,20 +28,20 @@ class BladeInlineScriptsProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../scripts/ColorSchemeSwitchTwoStates/js/init-script.js' => resource_path('js/color-scheme-switch-two-states/init-script.js'),
             __DIR__.'/../scripts/ColorSchemeSwitchTwoStates/js/switch-script.js' => resource_path('js/color-scheme-switch-two-states/switch-script.js'),
-        ], ['color-scheme-switch-2-states-js', 'color-scheme-switch-2-states-all']);
+        ], ['color-scheme-2-states-js', 'color-scheme-2-states-all']);
 
         $this->publishes([
             __DIR__.'/../tests/js/color-scheme-switch-two-states/init-script.test.js' => base_path('tests/js/color-scheme-switch-two-states/init-script.test.js'),
             __DIR__.'/../tests/js/color-scheme-switch-two-states/switch-script.test.js' => base_path('tests/js/color-scheme-switch-two-states/switch-script.test.js'),
-        ], ['color-scheme-switch-2-states-js-tests', 'color-scheme-switch-2-states-all']);
+        ], ['color-scheme-2-states-js-tests', 'color-scheme-2-states-all']);
 
         $this->publishes([
             __DIR__.'/../scripts/ColorSchemeSwitchTwoStates/view/hero-icons-tailwind-alpine.blade.php' => resource_path('views/color-scheme-switch-two-states/hero-icons-tailwind-alpine.blade.php'),
-        ], ['color-scheme-switch-2-states-views', 'color-scheme-switch-2-states-all']);
+        ], ['color-scheme-2-states-views', 'color-scheme-2-states-all']);
 
         $this->publishes([
             __DIR__.'/../scripts/ColorSchemeSwitchTwoStates/view/hero-icons-tailwind.blade.php' => resource_path('views/color-scheme-switch-two-states/hero-icons-tailwind.blade.php'),
-        ], ['color-scheme-switch-2-states-views', 'color-scheme-switch-2-states-all']);
+        ], ['color-scheme-2-states-views', 'color-scheme-2-states-all']);
     }
 
     public function publishColorScheme3States(): void
@@ -49,19 +49,19 @@ class BladeInlineScriptsProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../scripts/ColorSchemeSwitchThreeStates/js/init-script.js' => base_path('js/color-scheme-switch-three-states/init-script.js'),
             __DIR__.'/../scripts/ColorSchemeSwitchThreeStates/js/switch-script.js' => base_path('js/color-scheme-switch-three-states/switch-script.js'),
-        ], ['color-scheme-switch-3-states-js', 'color-scheme-switch-3-states-all']);
+        ], ['color-scheme-3-states-js', 'color-scheme-3-states-all']);
 
         $this->publishes([
             __DIR__.'/../tests/js/color-scheme-switch-three-states/init-script.test.js' => base_path('tests/js/color-scheme-switch-three-states/init-script.test.js'),
             __DIR__.'/../tests/js/color-scheme-switch-three-states/switch-script.test.js' => base_path('tests/js/color-scheme-switch-three-states/switch-script.test.js'),
-        ], ['color-scheme-switch-3-states-js-tests', 'color-scheme-switch-3-states-all']);
+        ], ['color-scheme-3-states-js-tests', 'color-scheme-3-states-all']);
 
         $this->publishes([
             __DIR__.'/../scripts/ColorSchemeSwitchThreeStates/view/hero-icons-tailwind-alpine.blade.php' => resource_path('views/color-scheme-switch-three-states/hero-icons-tailwind-alpine.blade.php'),
-        ], ['color-scheme-switch-3-states-views', 'color-scheme-switch-3-states-all']);
+        ], ['color-scheme-3-states-views', 'color-scheme-3-states-all']);
 
         $this->publishes([
             __DIR__.'/../scripts/ColorSchemeSwitchThreeStates/view/hero-icons-tailwind.blade.php' => resource_path('views/color-scheme-switch-three-states/hero-icons-tailwind.blade.php'),
-        ], ['color-scheme-switch-3-states-views', 'color-scheme-switch-3-states-all']);
+        ], ['color-scheme-3-states-views', 'color-scheme-3-states-all']);
     }
 }
