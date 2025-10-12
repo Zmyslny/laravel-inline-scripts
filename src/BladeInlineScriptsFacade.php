b429fe4eac0344161c518a6919dac8158770ae6f
Ostrowski Maciej <zmyslny@icloud.com>
2025-10-12 15:51:50 +0200

    chore: update BladeInlineScriptsFacade annotations

--- src/BladeInlineScriptsFacade.php
+++ src/BladeInlineScriptsFacade.php
@@ -8,7 +8,9 @@ use Illuminate\Support\Facades\Facade;
 use Zmyslny\LaravelInlineScripts\Contracts\RenderableScript;
 
 /**
- * @method static BladeInlineScriptsFacade take(RenderableScript ...$inlineScripts)
+ * @method static BladeInlineScriptsCore take(RenderableScript ...$scripts)
+ * @method static BladeInlineScriptsCore takeFile(string $path, array<string, string> $placeholders = [])
+ * @method static BladeInlineScriptsCore takeFiles(array< string, array<string,string> >|string ...$paths)
  */
 class BladeInlineScriptsFacade extends Facade
 {
